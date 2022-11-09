7e7393fdbc79ce707651ca97f98a31eeb0a0c820
dyhmzall <dyhmzall@gmail.com>
2022-11-09 20:49:34 +0300

    feat: unit and function tests

--- doc/test.md
+++ doc/test.md
@@ -16,3 +16,27 @@
 
 > symfony php bin/phpunit tests/Controller/ConferenceControllerTest.php
 
+#### установить бандл фикстуры
+
+> symfony composer req orm-fixtures --dev
+
+#### применение фикстур
+
+> symfony console doctrine:fixtures:load --env=test
+
+#### сброс фикстур
+
+> symfony console doctrine:fixtures:load --env=test
+
+#### запустить make-файл со всеми необходимыми командами
+
+> make tests
+
+#### установить приблуду, чтобы все результаты тестов откатывались после прохождения теста
+
+> symfony composer req "dama/doctrine-test-bundle:^6" --dev
+
+#### установить panther
+
+> symfony composer req panther --dev
+
--- tests/Controller/ConferenceControllerTest.php
+++ tests/Controller/ConferenceControllerTest.php
@@ -17,4 +17,34 @@ class ConferenceControllerTest extends WebTestCase
         $this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h2', 'Оставьте отзыв!');
     }
+
+    public function testConferencePage()
+    {
+        $client  = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertCount(2, $crawler->filter('h4'));
+
+        $client->clickLink('Подробнее');
+
+        $this->assertPageTitleContains('Paris');
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Paris 2018');
+        $this->assertSelectorExists('div:contains("Для этой конференции пока нет отзывов.")');
+    }
+
+    public function testCommentSubmission()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/conference/amsterdam-2019');
+        $client->submitForm('Submit', [
+            'comment_form[author]' => 'Fabien',
+            'comment_form[text]'   => 'Some feedback from an automated functional test',
+            'comment_form[email]'  => 'me@authomat.ed',
+            'comment_form[photo]'  => dirname(__DIR__, 2) . '/public/images/under-construction.gif',
+        ]);
+        $this->assertResponseRedirects();
+        $client->followRedirect();
+        $this->assertSelectorExists('div:contains("Всего 2 отзывов")');
+    }
 }
